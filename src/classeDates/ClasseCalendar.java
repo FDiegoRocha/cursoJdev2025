05df70ba5bef1991dece71c4855f231c8d62e2fa
Diego Rocha <fdiego.rs@gmail.com>
2025-12-09 14:01:16 -0300

    Classe Date e Calendar

---
+++ src/classeDates/ClasseCalendar.java
@@ -0,0 +1,42 @@
+package classeDates;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.Calendar;
+
+public class ClasseCalendar {
+
+	public static void main(String[] args) throws ParseException {
+		Calendar calendar = Calendar.getInstance();
+		
+		// Simular data que vem do banco de dados
+		
+		calendar.setTime(new SimpleDateFormat("dd-MM-yyyy").parse("10-03-2021"));
+		
+		calendar.add(Calendar.DAY_OF_MONTH, 5);
+		
+		System.out.println("Somando dia do mês: " + new SimpleDateFormat("dd-MM-yyyy").format(calendar.getTime()));
+		
+		calendar.add(Calendar.MONTH, 1);
+		
+		System.out.println("Somando o mês: " + new SimpleDateFormat("dd-MM-yyyy").format(calendar.getTime()));
+
+		calendar.add(Calendar.YEAR, 1);
+		
+		System.out.println("Somando 1 ano: " + new SimpleDateFormat("dd-MM-yyyy").format(calendar.getTime()));
+
+		
+		long quant = ChronoUnit.DAYS.between(LocalDate.parse("2025-01-01"), LocalDate.now());
+		
+		long quant2 = ChronoUnit.HOURS.between(LocalDateTime.parse("2025-12-09T11:40:00"), LocalDateTime.now());
+		
+		System.out.println("Possui " + quant + " dias entre a faixa de data");
+		
+		System.out.println("Possui " + quant2 + " horas entre a faixa de data");
+		
+	}
+
+}
---
+++ src/classeDates/DateJava.java
@@ -0,0 +1,67 @@
+package classeDates;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class DateJava {
+
+	public static void main(String[] args) throws ParseException {
+		
+		Calendar calendar = Calendar.getInstance();
+		Date date = new Date();
+		
+		System.out.println("Data em milisegundos: " + date.getTime());
+		System.out.println("Calendar Data em milisegundos: " + calendar.getTimeInMillis());
+		System.out.println("Dia do mês: " + date.getDate());
+		System.out.println("Calendar Dia do mês: " + calendar.get(Calendar.DAY_OF_MONTH));
+		System.out.println("Dia da semana: " + date.getDay());
+		System.out.println("Calendar Dia da semana: " + calendar.get(Calendar.DAY_OF_WEEK));
+		System.out.println("Hora do dia: " + date.getHours());
+		System.out.println("Calendar Hora do dia: " + calendar.get(Calendar.HOUR_OF_DAY));
+		System.out.println("Minuto da hora: " + date.getMinutes());
+		System.out.println("Calendar Minuto da hora: " + calendar.get(Calendar.MINUTE));
+		System.out.println("Secundos: " + date.getSeconds());
+		System.out.println("Calendar Secundos: " + calendar.get(Calendar.SECOND));
+		System.out.println("Ano: " + (date.getYear() + 1900));
+		System.out.println("Calendar Ano: " + calendar.get(Calendar.YEAR) );
+
+		// ---------------- Simple Date Format -----------------------
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm.ss");
+		
+		System.out.println("Data atual em formato padrão e string: " + sdf.format(date));
+		
+		System.out.println("Calendar Data atual em formato padrão e string: " + sdf.format(calendar.getTime()));
+		
+		SimpleDateFormat sdf1 = new SimpleDateFormat("yyyy-MM-dd HH:mm.ss");
+
+		System.out.println("Data atual em formato banco de dados: " + sdf1.format(date));
+		
+		SimpleDateFormat sdf2 = new SimpleDateFormat("dd/MM/yyyy");
+		
+		System.out.println("Objeto Date: " + sdf2.parse("11/10/1994"));
+
+		// ---------------------- Before e Afte -------------------------------
+		
+		SimpleDateFormat sdf3 = new SimpleDateFormat("dd/MM/yyyy");
+		
+		Date dataVencimento = sdf3.parse("10/04/2021");
+		Date dataAtualHoje = sdf3.parse("11/04/2021");
+		
+		if(dataVencimento.after(dataAtualHoje)) {
+			System.out.println("Boleto vencido - Urgente!");
+		}else {
+			System.out.println("Boleto não vencido");
+		}
+		
+		if(dataVencimento.before(dataAtualHoje)) {
+			System.out.println("Boleto não vencido");
+		}else {
+			System.out.println("Boleto vencido - Urgente!");
+		}
+
+	}
+
+}
