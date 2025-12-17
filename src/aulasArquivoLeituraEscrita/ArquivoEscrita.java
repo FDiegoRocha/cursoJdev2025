7f01142ea50fbdaa6071d949ac0fe50ce8e3ef05
Diego Rocha <fdiego.rs@gmail.com>
2025-12-17 13:57:26 -0300

    Classe de leitura e escrita arquivos

---
+++ src/aulasArquivoLeituraEscrita/ArquivoEscrita.java
@@ -0,0 +1,41 @@
+package aulasArquivoLeituraEscrita;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArquivoEscrita {
+
+	public static void main(String[] args) throws IOException {
+		Pessoa pessoa1 = new Pessoa("Fernando Diego", "fernando@gmail.com", 37);
+		Pessoa pessoa2 = new Pessoa("Maria Julia", "julia@gmail.com", 23);
+		Pessoa pessoa3 = new Pessoa("Fabio Junior", "fabio@gmail.com", 55);
+		
+		// Pode vir do banco de dados ou qualquer fonte de dados
+		List<Pessoa> pessoas = new ArrayList<>();
+		pessoas.add(pessoa1);
+		pessoas.add(pessoa2);
+		pessoas.add(pessoa3);
+		
+		// Objeto do tipo File
+		File arquivo = new File("C:\\cursoJdev2025\\src\\aulasArquivoLeituraEscrita\\arquivo.txt");
+		
+		// Se não existir cria o arquivo no caminho informado acima
+		if(!arquivo.exists()) {
+			arquivo.createNewFile();
+		}
+		
+		// instancia um objeto para escrever no arquivo
+		FileWriter escreverArquivo = new FileWriter(arquivo);
+		
+		for(Pessoa p : pessoas) {
+			escreverArquivo.write(p.getNome() + ";" + p.getEmail() + ";" + p.getIdade() + "\n");
+		}
+		
+		escreverArquivo.flush();
+		escreverArquivo.close();
+	}
+
+}
---
+++ src/aulasArquivoLeituraEscrita/LeituraArquivos.java
@@ -0,0 +1,42 @@
+package aulasArquivoLeituraEscrita;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class LeituraArquivos {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		
+		FileInputStream arquivo = new FileInputStream(new File("C:\\cursoJdev2025\\src\\aulasArquivoLeituraEscrita\\arquivo.txt"));
+		
+		Scanner leituraArquivo = new Scanner(arquivo, "UTF-8");
+		
+		List<Pessoa> pessoas = new ArrayList<>();
+		
+		while(leituraArquivo.hasNext()) {
+			
+			String linha = leituraArquivo.nextLine();
+			
+			if(linha != null && !linha.isEmpty()) {
+				String[] dados = linha.split(";");
+				
+				Pessoa pessoa = new Pessoa();
+				pessoa.setNome(dados[0]);
+				pessoa.setEmail(dados[1]);
+				pessoa.setIdade(Integer.parseInt(dados[2]));
+				pessoas.add(pessoa);
+			}
+		}
+		
+		for(Pessoa p : pessoas) {
+			System.out.println(p);
+		}
+		
+		leituraArquivo.close();
+	}
+
+}
